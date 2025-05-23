cd0118162650b2bcc57edf647d51336158b4a1a6
sriharsha <sriharshadabbiru04@gmail.com>
2025-05-24 00:52:39 +0530

    added logic to qury contacts

--- src/routes/identify.js
+++ src/routes/identify.js
@@ -7,7 +7,17 @@ router.post("/", async (req, res) => {
   if (!email && !phoneNumber) {
     return res.status(400).json({ error: "Email or phone number is required" });
   }
-  res.json({ message: "Input received", email, phoneNumber });
+
+  try {
+    const { rows: existingContacts } = await pool.query(
+      "SELECT * FROM contact WHERE email = $1 OR phoneNumber = $2",
+      [email, phoneNumber]
+    );
+    res.json({ message: "Matching contacts found ", existingContacts });
+  } catch (error) {
+    console.error("Error identifying contacts", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 module.exports = router;
