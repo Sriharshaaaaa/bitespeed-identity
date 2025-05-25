1c36eee54d8af4ba37f2a59b022e37442cda0e9b
sriharsha <sriharshadabbiru04@gmail.com>
2025-05-25 11:44:00 +0530

    typo

--- src/db/index.js
+++ src/db/index.js
@@ -14,7 +14,7 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DB_URL,
   ssl: {
     rejectUnauthorized: false, // required for Neon and some other hosts
   },
